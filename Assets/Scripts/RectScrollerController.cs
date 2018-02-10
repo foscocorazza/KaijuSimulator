af80535283914d4ff9290b11e03fa49eb434726e
Simone Chelo <chelosimone@gmail.com>
2018-02-10 11:32:26 +0100

    Title scene

---
+++ Assets/Scripts/RectScrollerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class RectScrollerController : MonoBehaviour {
+
+	private Vector2 startPosition;
+	private RectTransform rectTransform;
+	private Rect canvasRect;
+	public Vector2 Speed;
+
+	void Start () {
+		rectTransform = GetComponent<RectTransform> ();
+		startPosition = rectTransform.anchoredPosition;
+		canvasRect = GetComponentInParent<Canvas>().GetComponent<RectTransform> ().rect;
+	}
+
+	void Update () {
+		rectTransform.anchoredPosition += Speed * Time.deltaTime;
+
+		if (Speed.x < 0 && rectTransform.anchoredPosition.x <= -(rectTransform.rect.width - canvasRect.width)) {
+			rectTransform.anchoredPosition = new Vector2(startPosition.x, rectTransform.anchoredPosition.y);
+		}
+			
+	}
+}
