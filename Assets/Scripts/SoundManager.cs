91a7837a6200b1cd767abf9d87b8c30358c81a38
Leonard Maxim <leonard.maxim@outlook.com>
2018-02-10 23:50:54 +0100

    added sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundManager : MonoBehaviour {
+    private static SoundManager _instance;
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<SoundManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    [System.Serializable]
+    public class sound
+    {
+        public string name;
+        public AudioClip clip;
+    }
+
+    public List<sound> sounds;
+
+    public AudioClip GetSound(string name)
+    {
+        foreach (sound s in sounds)
+        {
+            if (s.name == name)
+            {
+                return s.clip;
+            }
+        }
+        return null;
+    }
+}
