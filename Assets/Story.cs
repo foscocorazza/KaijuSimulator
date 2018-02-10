d6ce1bafd6fb7710c954e52867d8b83954d717db
Christian Carvelli <chca@itu.dk>
2018-02-10 22:24:43 +0100

    - 3D buildings

---
+++ Assets/Story.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story : MonoBehaviour {
+    private Material mat;
+
+    private void Awake() {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetTint (Color c) {
+        mat.SetColor("_Color", c);
+    }
+}
