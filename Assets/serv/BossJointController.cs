82b53d3f8e3f43ff155427c22687962aa24a9ffe
ValD <vucket07@gmail.com>
2018-02-10 11:27:05 +0100

    ragdoll test

---
+++ Assets/serv/BossJointController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossJointController : MonoBehaviour {
+
+    private HingeJoint myHinge;
+    private JointSpring mySpring;
+
+    void Awake() {
+        myHinge = GetComponent<HingeJoint>();
+        mySpring = myHinge.spring;
+    }
+
+    public void updateTarget(float newAngle) {
+        //Formatting the angle
+        if (newAngle > 180) {
+            newAngle -= 360;
+        }
+        newAngle = Mathf.Clamp(newAngle, myHinge.limits.min, myHinge.limits.max);
+        mySpring.targetPosition = newAngle;
+        myHinge.spring = mySpring;
+    }
+}
---
+++ Assets/serv/PhysicsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsTest : MonoBehaviour {
+    public Vector3 direction;
+    public float force;
+
+    private Rigidbody myBody;
+
+	void Start () {
+        myBody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            myBody.AddForce(direction * force);
+        }
+	}
+}
