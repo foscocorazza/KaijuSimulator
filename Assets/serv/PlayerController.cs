7b1789321db065793855abeae9e6b936c1574159
Christian Carvelli <chca@itu.dk>
2018-02-10 15:58:14 +0100

    - interface to control building destruction - tests Boss mat/shading

--- Assets/serv/PlayerController.cs
+++ Assets/serv/PlayerController.cs
@@ -15,13 +15,13 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update () {
-        float p1AxisArmX = Input.GetAxis("P1HorizontalAxis1");
+        /*float p1AxisArmX = Input.GetAxis("P1HorizontalAxis1");
         float p1AxisArmY = Input.GetAxis("P1VerticalAxis1");
         Debug.Log(p1AxisArmX+ " " + p1AxisArmY);
         float p1AxisLegX = Input.GetAxis("P1HorizontalAxis2");
         float p1AxisLegY = Input.GetAxis("P1VerticalAxis2");
         Debug.Log(p1AxisLegX + " " + p1AxisLegY);
-
+        */
         /*        
         float p1AxisArmX = Input.GetAxis("P1HorizontalAxis1");
         float p1AxisArmY = Input.GetAxis("P1VerticalAxis1");
