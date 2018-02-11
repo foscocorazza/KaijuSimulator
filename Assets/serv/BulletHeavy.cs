8032f386bc9bc619203bb392830d70de3a7c9571
ValD <vucket07@gmail.com>
2018-02-11 11:42:35 +0100

    true kaiju

--- Assets/serv/BulletHeavy.cs
+++ Assets/serv/BulletHeavy.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class BulletHeavy : MonoBehaviour {
 
     public float force = 30;
+    public float lifeTime = 0.9f;
 
     private Rigidbody myBody;
 
 	void Start () {
         myBody = GetComponent<Rigidbody>();
         myBody.AddForce(transform.right * force, ForceMode.Impulse);
+        StartCoroutine(ggBullet());
+    }
+
+    private IEnumerator ggBullet() {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
     }
 }
